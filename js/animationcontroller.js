e959fe75e9a7561fdd7e2a07fb0bb2115cf75437
Amjad <ptamjadaju@gmail.com>
2021-03-25 16:28:58 +0530

    animations added + navbar styles

---
+++ js/animationcontroller.js
@@ -0,0 +1,57 @@
+function check_if_in_view() {
+    var $window = $(window);
+    var $animation_elements0 = $('.animation-element-0');
+    var $animation_elements = $('.animation-element');
+    var $animation_elements2 = $('.animation-element-2');
+
+    var window_height = $window.height();
+    var window_top_position = $window.scrollTop();
+    var window_bottom_position = (window_top_position + window_height);
+  
+    $.each($animation_elements, function() {
+      var $element = $(this);
+      var element_height = $element.outerHeight();
+      var element_top_position = $element.offset().top;
+      var element_bottom_position = (element_top_position + element_height);
+  
+      //check to see if this current container is within viewport
+      if ((element_bottom_position >= window_top_position) &&
+          (element_top_position <= window_bottom_position)) {
+        $element.addClass('upAnimation2');
+      } else {
+        
+      }
+    });
+
+    $.each($animation_elements2, function() {
+        var $element = $(this);
+        var element_height = $element.outerHeight();
+        var element_top_position = $element.offset().top;
+        var element_bottom_position = (element_top_position + element_height);
+    
+        //check to see if this current container is within viewport
+        if ((element_bottom_position >= window_top_position) &&
+            (element_top_position <= window_bottom_position)) {
+          $element.addClass('slideAnimations');
+        } else {
+          
+        }
+      });
+
+      $.each($animation_elements0, function() {
+        var $element = $(this);
+        var element_height = $element.outerHeight();
+        var element_top_position = $element.offset().top;
+        var element_bottom_position = (element_top_position + element_height);
+    
+        //check to see if this current container is within viewport
+        if ((element_bottom_position >= window_top_position) &&
+            (element_top_position <= window_bottom_position)) {
+          $element.addClass('zoomAnimation');
+        } else {
+          
+        }
+      });
+
+
+  }
